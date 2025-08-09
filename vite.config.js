d66ea11427fbbb9517be905ad952e09b5733eddf
Carl Jasper Ramos <zoraao79@gmail.com>
2025-08-09 18:33:30 +0800

    new

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/laptop_recommendation/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
